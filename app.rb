fccf3a8102323df54a252020d520bc3e1717e8bc
Macey <macebake@gmail.com>
2016-04-25 12:31:34 +0100

    Legit cat picture on lock

--- app.rb
+++ app.rb
@@ -5,7 +5,6 @@ get '/' do
   "fjdsiaofjdsia"
 end
 
-
 get '/secret' do
   "SECRETS!!"
 end
@@ -16,4 +15,8 @@ end
 
 get '/alex' do
   "alex 4eva"
+end
+
+get '/cat' do
+  "<img src='http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png' style='border:3px dashed red'>"
 end
