41a44d1d5562b42d4fc28c58d23f211fad166cd9
Macey <macebake@gmail.com>
2016-04-25 17:04:15 +0100

    Messing with capybara

--- app.rb
+++ app.rb
@@ -24,8 +24,13 @@ get '/random-cat' do
 
 end
 
-get '/named-cat' do
+get '/name-form' do
+  erb(:form)
+end
+
+post '/named-cat' do
   @cat_name = params[:cat_name]
+  p params
   @cat_array = ["Banana", "Fergus", "Ron Burgundy"].sample
   @cat_pics = ['https://s-media-cache-ak0.pinimg.com/236x/09/0e/1a/090e1a865716e97ebb1f6c465c3f5aa6.jpg', 'http://40.media.tumblr.com/c99e86b185193e47ae9f63b8509db218/tumblr_nxpxn5RSXU1u0wbuto1_400.jpg', 'http://buzzsharer.com/wp-content/uploads/2015/07/Scottish-Fold-face-eyes.jpg', 'http://pawesomecats.com/wp-content/uploads/2014/02/Scottish-Fold-Cat.jpg', 'https://s-media-cache-ak0.pinimg.com/236x/1d/28/40/1d284065506dd39eabfa605821c3d4a8.jpg'].sample
   erb(:index)
