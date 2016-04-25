c0d02321b5cd5156c3377d48bf6746c4d7e9cf09
Macey <macebake@gmail.com>
2016-04-25 12:03:14 +0100

    Playing with Sinatra

---
+++ app.rb
@@ -0,0 +1,19 @@
+require 'sinatra'
+require 'shotgun'
+
+get '/' do
+  "fjdsiaofjdsia"
+end
+
+
+get '/secret' do
+  "SECRETS!!"
+end
+
+get '/macey' do
+  "macey 4eva"
+end
+
+get '/alex' do
+  "alex 4eva"
+end
